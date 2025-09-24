1992898af978d16cc0174824e62543271fbc1936
misha <mictekaras29@gmail.com>
2025-09-24 18:45:56 +0700

    Update

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ class TaskSchema(BaseModel):
     completed: bool = False
 
 tasks = []
-
-
+tasks_id_counter = 1
 
 @app.get("/tasks")
 def get_tasks():
@@ -19,19 +18,40 @@ def get_tasks():
 
 @app.post("/tasks")
 def create_task(task: TaskSchema):
-    task.id = tasks_id_counter
+    global tasks_id_counter 
+    new_task = TaskSchema(
+        id=tasks_id_counter,
+        text=task.text,
+        completed=task.completed
+    )
     tasks_id_counter += 1
-    tasks.append(task)
-    return {"message": "Задача добавлена"}
+    tasks.append(new_task)
+    return {"message": "Задача добавлена", "task": new_task}
 
-@app.delete(f"/tasks/{id}")
+@app.put("/tasks/{id}")
+def update_task(id: int, updated_task: TaskSchema):
+    for task in tasks:
+        if task.id == id:
+            task.text = updated_task.text
+            task.completed = updated_task.completed
+            return {"message": "Задача обновлена", "task": task}
+    return {"message": "Задача не найдена"}
+
+@app.patch("/tasks/{id}/toggle")
+def SwitchCompleted(id: int):
+    for task in tasks:
+        if task.id == id:
+            task.completed = not task.completed
+            return {"message": "Статус задачи изменен", "task": task}
+    return {"message": "Задача не найдена"}
+
+@app.delete("/tasks/{id}")
 def delete_task(id: int):
     for task in tasks:
         if task.id == id:
-            tasks.pop(id)
+            tasks.remove(task)
             return {"message": "Задача удалена"}
     return {"message": "Задача не найдена"}
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
